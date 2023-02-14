4408ca452fef32b5cc00c1de7f8fbfa6e66f6514
gustavotrr <gustavotrr@gmail.com>
2023-02-13 22:41:36 -0300

    Creating the Autocomplete component implementation

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,5 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
+
+const apiUrl = 'http://localhost:8000/api';
 
 export default function Autocomplete() {
-    return <h1>Autocomplete</h1>
+    const [inputValue, setInputValue] = useState('');
+    const [suggestions, setSuggestions] = useState<string[]>([]);
+    
+    const getData = async (endpoint: string) => {
+        const response = await fetch(endpoint)
+        const data = await response.json()
+        return data
+      }
+
+      const handleInputChange = async (event: any) => {
+        const newValue = event.target.value;
+        setInputValue(newValue);
+        if (!newValue) {
+            setSuggestions([]);
+            return
+        }
+        const fullUrl = `${apiUrl}/autocomplete?q=${newValue}`;
+        const response = await getData(fullUrl)
+        setSuggestions(response)
+        return
+     }
+
+    return (
+        <div>
+            <h1>Autocomplete</h1>
+            <input
+                type="text"
+                value={inputValue}
+                onChange={handleInputChange}
+                list="suggestions"
+                autoFocus
+                placeholder='Type the word here'
+            />
+            <datalist id="suggestions">
+                {suggestions.map((suggestion) => (
+                    <option value={suggestion} key={suggestion} />
+                ))}
+            </datalist>
+        </div>
+    );
 }
